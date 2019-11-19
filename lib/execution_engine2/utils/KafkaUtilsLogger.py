01d9b07e2cdc71c34d56357d4a686bd231c60379
Boris Sadkhin <boris@bortech.us>
2019-11-18 18:49:59 -0600

    Added Kafka + code reformat

--- lib/execution_engine2/utils/KafkaUtilsLogger.py
+++ lib/execution_engine2/utils/KafkaUtilsLogger.py
@@ -6,6 +6,7 @@ import logging
 import sys
 
 from confluent_kafka import Producer, KafkaError, Consumer
+from kafka import KafkaProducer
 
 _TOPICS = ["ee2"]
 
@@ -59,3 +60,46 @@ def send_message_to_kafka(data, topic="ee2"):
     producer = Producer({"bootstrap.servers": "kafka"})
     producer.produce(topic, json.dumps(data), callback=_delivery_report)
     producer.poll(2)
+
+
+class KafkaHandler(logging.Handler):
+    """Class to instantiate the kafka logging facility."""
+
+    def __init__(self, hostlist, topic="corp_it_testing", tls=None):
+        """Initialize an instance of the kafka handler."""
+        logging.Handler.__init__(self)
+        self.producer = KafkaProducer(
+            bootstrap_servers=hostlist,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+            linger_ms=10,
+        )
+        self.topic = topic
+
+    def emit(self, record):
+        """Emit the provided record to the kafka_client producer."""
+        # drop kafka logging to avoid infinite recursion
+        if "kafka." in record.name:
+            return
+
+        try:
+            # apply the logger formatter
+            msg = self.format(record)
+            self.producer.send(self.topic, {"message": msg})
+            self.flush(timeout=1.0)
+        except Exception:
+            logging.Handler.handleError(self, record)
+
+    def flush(self, timeout=None):
+        """Flush the objects."""
+        self.producer.flush(timeout=timeout)
+
+    def close(self):
+        """Close the producer and clean up."""
+        self.acquire()
+        try:
+            if self.producer:
+                self.producer.close()
+
+            logging.Handler.close(self)
+        finally:
+            self.release()
