3d497723a857f8e8871a7c90ed6580b4fb4038a0
Tianhao-Gu <tgu@anl.gov>
2019-11-24 22:57:11 -0600

    add get/add_job_logs, check_job_canceled, check_jobs

--- test/ee2_load_test.py
+++ test/ee2_load_test.py
@@ -5,6 +5,7 @@ import os
 import unittest
 import threading
 import queue
+import time
 from configparser import ConfigParser
 from unittest.mock import patch
 
@@ -224,7 +225,7 @@ class ee2_SDKMethodRunner_test(unittest.TestCase):
     @patch.object(SDKMethodRunner, "_get_client_groups", return_value="a group")
     @patch.object(SDKMethodRunner, "_get_module_git_commit", return_value="a version")
     @patch.object(Condor, "run_job", return_value=submission_info(clusterid="test", submit="job", error=None))
-    def test_run_job(self, can_write, _get_client_groups, _get_module_git_commit, run_job):
+    def test_run_job_stress(self, can_write, _get_client_groups, _get_module_git_commit, run_job):
         """
         testing running 3 different jobs in multiple theads.
         """
@@ -343,5 +344,222 @@ class ee2_SDKMethodRunner_test(unittest.TestCase):
                 self.assertEqual(job.to_mongo().to_dict().get('status'), 'completed')
 
             jobs = self.mongo_util.get_jobs(job_ids=(job_ids_queued + job_ids_running + job_ids_completed))
+            jobs.delete()
+            self.assertEqual(ori_job_count, Job.objects.count())
+
+    def test_check_jobs_stress(self):
+        """
+        testing check jobs in multiple theads.
+        """
+
+        thread_count = self.thread_count  # threads to test
+
+        with self.mongo_util.mongo_engine_connection():
+            ori_job_count = Job.objects.count()
+            runner = self.getRunner()
+
+            # set job method differently to distinguish
+            method_1 = 'a_method'
+            method_2 = 'b_method'
+            job_params_1 = self.get_sample_job_params(method=method_1)
+            job_params_2 = self.get_sample_job_params(method=method_2)
+
+            # create jobs
+            job_id_1 = runner._init_job_rec(self.user_id, job_params_1)
+            job_id_2 = runner._init_job_rec(self.user_id, job_params_2)
+
+            threads = list()
+            job_status = list()
+            que = queue.Queue()
+
+            # execute check_jobs in multiple threads
+            for index in range(thread_count):
+                x = threading.Thread(target=que.put(
+                    self.impl.check_jobs(ctx=self.ctx, params={'job_ids': [job_id_1, job_id_2]})))
+                threads.append(x)
+                x.start()
+
+            for index, thread in enumerate(threads):
+                thread.join()
+
+            while not que.empty():
+                job_status.append(que.get())
+
+            # exam returned job status
+            for job_status in job_status:
+                job_status = job_status[0]['job_states']
+                job_ids = [js['job_id'] for js in job_status]
+                job_methods = [js['job_input']['method'] for js in job_status]
+                self.assertCountEqual(job_ids, [job_id_1, job_id_2])
+                self.assertCountEqual(job_methods, [method_1, method_2])
+
+            jobs = self.mongo_util.get_jobs(job_ids=[job_id_1, job_id_2])
+            jobs.delete()
+            self.assertEqual(ori_job_count, Job.objects.count())
+
+    def test_check_job_canceled_stress(self):
+        """
+        testing check_job_canceled in multiple theads.
+        """
+
+        thread_count = self.thread_count  # threads to test
+
+        with self.mongo_util.mongo_engine_connection():
+            ori_job_count = Job.objects.count()
+            runner = self.getRunner()
+
+            job_params = self.get_sample_job_params()
+
+            # create jobs
+            job_id_running = runner._init_job_rec(self.user_id, job_params)
+            job_id_terminated = runner._init_job_rec(self.user_id, job_params)
+            job_id_completed = runner._init_job_rec(self.user_id, job_params)
+
+            self.impl.update_job_status(ctx=self.ctx, params={'job_id': job_id_running, 'status': 'running'})
+            self.impl.update_job_status(ctx=self.ctx, params={'job_id': job_id_terminated, 'status': 'terminated'})
+            self.impl.update_job_status(ctx=self.ctx, params={'job_id': job_id_completed, 'status': 'completed'})
+
+            threads = list()
+            job_canceled_status = list()
+            que = queue.Queue()
+
+            # execute check_job_canceled in multiple threads
+            for index in range(thread_count):
+                x = threading.Thread(target=que.put(
+                    self.impl.check_job_canceled(ctx=self.ctx, params={'job_id': job_id_running})))
+                threads.append(x)
+                x.start()
+
+                y = threading.Thread(target=que.put(
+                    self.impl.check_job_canceled(ctx=self.ctx, params={'job_id': job_id_terminated})))
+                threads.append(y)
+                y.start()
+
+                z = threading.Thread(target=que.put(
+                    self.impl.check_job_canceled(ctx=self.ctx, params={'job_id': job_id_completed})))
+                threads.append(z)
+                z.start()
+
+            for index, thread in enumerate(threads):
+                thread.join()
+
+            while not que.empty():
+                job_canceled_status.append(que.get())
+
+            # exam correct job ids returned
+            job_ids_returned = [jcs_return[0]['job_id']for jcs_return in job_canceled_status]
+            self.assertEqual(len(job_ids_returned), thread_count * 3)  # exam total job number returned
+            self.assertEqual(job_ids_returned.count(job_id_running), thread_count)
+            self.assertEqual(job_ids_returned.count(job_id_terminated), thread_count)
+            self.assertEqual(job_ids_returned.count(job_id_completed), thread_count)
+
+            # exam returned job canceled status
+            for job_canceled_status_return in job_canceled_status:
+                job_canceled_status_return = job_canceled_status_return[0]
+                if job_canceled_status_return['job_id'] == job_id_running:
+                    self.assertFalse(job_canceled_status_return['canceled'])
+                    self.assertFalse(job_canceled_status_return['finished'])
+                if job_canceled_status_return['job_id'] == job_id_terminated:
+                    self.assertTrue(job_canceled_status_return['canceled'])
+                    self.assertTrue(job_canceled_status_return['finished'])
+                if job_canceled_status_return['job_id'] == job_id_completed:
+                    self.assertFalse(job_canceled_status_return['canceled'])
+                    self.assertTrue(job_canceled_status_return['finished'])
+
+            jobs = self.mongo_util.get_jobs(job_ids=[job_id_running, job_id_terminated, job_id_completed])
+            jobs.delete()
+            self.assertEqual(ori_job_count, Job.objects.count())
+
+    def test_get_job_logs_stress(self):
+        """
+        testing get_job_logs in multiple theads.
+        """
+
+        thread_count = self.thread_count  # threads to test
+
+        with self.mongo_util.mongo_engine_connection():
+            ori_job_count = Job.objects.count()
+            runner = self.getRunner()
+
+            # create job
+            job_id = runner._init_job_rec(self.user_id, self.get_sample_job_params())
+
+            # add one line to job
+            ts = time.time()
+            job_line = [{'line': 'hello ee2', 'is_error': True, 'ts': ts}]
+            self.impl.add_job_logs(ctx=self.ctx, job_id=job_id, lines=job_line)
+
+            threads = list()
+            job_lines = list()
+            que = queue.Queue()
+
+            # execute get_job_logs in multiple threads
+            for index in range(thread_count):
+                x = threading.Thread(target=que.put(
+                    self.impl.get_job_logs(ctx=self.ctx, params={'job_id': job_id})))
+                threads.append(x)
+                x.start()
+
+            for index, thread in enumerate(threads):
+                thread.join()
+
+            while not que.empty():
+                job_lines.append(que.get())
+
+            self.assertEqual(len(job_lines), thread_count)  # exam total number of job lines returned
+
+            # exam each get_job_logs result
+            for job_line in job_lines:
+                job_line = job_line[0]['lines'][0]
+                self.assertEqual(job_line['line'], 'hello ee2')
+                self.assertEqual(job_line['linepos'], 1)
+                self.assertFalse(job_line['error'])
+                self.assertEqual(job_line['ts'], int(ts * 1000))
+
+            jobs = self.mongo_util.get_jobs(job_ids=[job_id])
+            jobs.delete()
+            self.assertEqual(ori_job_count, Job.objects.count())
+
+    def test_add_job_logs_stress(self):
+        """
+        testing add_job_logs in multiple theads.
+        """
+
+        thread_count = self.thread_count  # threads to test
+
+        with self.mongo_util.mongo_engine_connection():
+            ori_job_count = Job.objects.count()
+            runner = self.getRunner()
+
+            # create job
+            job_id = runner._init_job_rec(self.user_id, self.get_sample_job_params())
+
+            # job line to be added
+            ts = time.time()
+            job_line = [{'line': 'hello ee2', 'is_error': True, 'ts': ts}]
+
+            threads = list()
+            # execute add_job_logs in multiple threads
+            for index in range(thread_count):
+                x = threading.Thread(target=self.impl.add_job_logs(ctx=self.ctx, job_id=job_id, lines=job_line))
+                threads.append(x)
+                x.start()
+
+            for index, thread in enumerate(threads):
+                thread.join()
+
+            job_lines = self.impl.get_job_logs(ctx=self.ctx, params={'job_id': job_id})[0]
+
+            self.assertEqual(job_lines['last_line_number'], thread_count)  # exam total number of job lines created by add_job_logs
+
+            # exam each line created by add_job_logs
+            lines = job_lines['lines']
+            self.assertEqual(len(lines), thread_count)
+            for line in lines:
+                self.assertEqual(line['line'], 'hello ee2')
+                self.assertFalse(line['error'])
+                self.assertEqual(line['ts'], int(ts * 1000))
+
+            jobs = self.mongo_util.get_jobs(job_ids=[job_id])
             jobs.delete()
             self.assertEqual(ori_job_count, Job.objects.count())
