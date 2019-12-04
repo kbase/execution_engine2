2ef3fdcbb5f4e0445fc61aec784fe3f721c9db09
Boris Sadkhin <boris@bortech.us>
2019-12-03 23:06:57 -0600

    Added tests

--- lib/execution_engine2/utils/KafkaUtils.py
+++ lib/execution_engine2/utils/KafkaUtils.py
@@ -3,59 +3,42 @@
 
 import json
 import logging
-import sys
 
-from confluent_kafka import Producer, KafkaError, Consumer
+logging.basicConfig(level=logging.INFO)
+from dataclasses import dataclass
 
-_TOPICS = ["ee2"]
+from confluent_kafka import Producer
+
+KAFKA_EVENT_TYPE = "status_update"
+DEFAULT_TOPIC = "ee2"
 
 
-def _test_sample_consumer():
-    """
-    This is for testing
-    :return:
-    """
-    consumer = Consumer(
-        {
-            "bootstrap.servers": "kafka:9096",
-            "group.id": sys.argv[1],
-            "auto.offset.reset": "earliest",
-        }
-    )
-    topics = ["ee2"]
-    consumer.subscribe(_TOPICS)
-    print("Consuming from", topics)
-    while True:
-        msg = consumer.poll(0.5)
-        if msg is None:
-            continue
-        if msg.error():
-            if msg.error().code() == KafkaError._PARTITION_EOF:
-                print("Reached end of the stream.")
-            else:
-                print(f"Error: {msg.error()}")
-                continue
-        print(f"New message: {msg.value().decode('utf-8')}")
-    consumer.close()
+@dataclass
+class KafkaStatusUpdate:
+    job_id: str
+    previous_status: str
+    new_status: str
+    topic: str = DEFAULT_TOPIC
+    event_type: str = KAFKA_EVENT_TYPE
+    error: bool = False
 
 
 def _delivery_report(err, msg):
     if err is not None:
-        print("Message delivery failed:", err)
+        msg = "Message delivery failed:", err
+        print(msg)
+        logging.error(msg)
     else:
-        print(f"Message delivered to topic '{msg.topic()}': {msg.value()}")
+        msg = f"Message delivered to topic '{msg.topic()}': {msg.value()}"
+        print(msg)
+        logging.error(msg)
 
 
-def send_message_to_kafka(data, topic="ee2"):
-    required_keys = ["job_id", "new_status", "previous_status"]
-    errors = 0
-    for item in required_keys:
-        if item not in data.keys():
-            logging.error(f"You need to include {item} in your kafka message")
-            errors += 1
-    # if errors > 0:
-    #     raise Exception("Malformed kafka message")
+def send_message_to_kafka(
+    data, topic=DEFAULT_TOPIC, event_type=KAFKA_EVENT_TYPE, server_address="kafka"
+):
 
-    producer = Producer({"bootstrap.servers": "kafka"})
-    producer.produce(topic, json.dumps(data), callback=_delivery_report)
+    message = KafkaStatusUpdate(**data)
+    producer = Producer({"bootstrap.servers": server_address})
+    producer.produce(topic, json.dumps(message.__dict__), callback=_delivery_report)
     producer.poll(2)
---
+++ test/ee2_kafka_test.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""Module to test out logging to kafka."""
+
+import json
+import logging
+import unittest
+
+from execution_engine2.utils.KafkaUtils import send_message_to_kafka
+from test.utils.KafkaTestUtils import Consumer, KafkaError
+
+logging.basicConfig(level=logging.DEBUG)
+
+from unittest.mock import patch
+from io import StringIO
+import copy
+import time
+
+
+class ExecutionEngine2SchedulerTest(unittest.TestCase):
+
+    # TODO Integration Test with EE2/KAFKA
+    # TODO Test to make sure that the correct messages are being sent from the various methods
+
+    def test_produce_and_consume(self):
+        """
+        Send a message via the kafka utils
+        and test that the producer callback function works
+        :return:
+        """
+        data = {"job_id": "123", "previous_status": "running", "new_status": "created"}
+        data2 = copy.copy(data)
+        data2["error"] = True
+
+        with patch("sys.stdout", new=StringIO()) as fakeOutput:
+            send_message_to_kafka(data)
+            send_message_to_kafka(data2)
+            stdout = fakeOutput.getvalue().strip()
+            self.assertIn(f"Message delivered to topic 'ee2':", stdout)
+
+        for k, v in data.items():
+            self.assertIn(k, stdout)
+            self.assertIn(v, stdout)
+
+        # Is order assured?
+        last_msg = self._consume_last("ee2", "ee2")
+        for key in data.keys():
+            value = data[key]
+            self.assertIn(key, last_msg)
+            data_value = last_msg[key]
+            self.assertEquals(value, data_value)
+
+        self.assertIn("error", last_msg)
+
+        last_msg2 = self._consume_last("ee2", "ee2")
+        for key in data2.keys():
+            value2 = data2[key]
+            self.assertIn(key, last_msg2)
+            data_value2 = last_msg2[key]
+            self.assertEquals(value2, data_value2)
+
+        self.assertIn("error", last_msg2)
+
+    @staticmethod
+    def _consume_last(topic, key, timeout=10):
+        """Consume the most recent message from the topic stream."""
+        consumer = Consumer(
+            {
+                "bootstrap.servers": "kafka",
+                "group.id": "test_only",
+                "auto.offset.reset": "earliest",
+            }
+        )
+        consumer.subscribe([topic])
+        start_time = time.time()
+        while True:
+            # get time elapsed in seconds
+            elapsed = time.time() - start_time
+            if elapsed > timeout:
+                # close consumer before throwing error
+                consumer.close()
+                raise TimeoutError(
+                    f"Error: Consumer waited past timeout of {timeout} seconds"
+                )
+            msg = consumer.poll(0.5)
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    print("End of stream.")
+                else:
+                    print(f"Error: {msg.error()}")
+                continue
+            if msg:
+                consumer.close()
+                return json.loads(msg.value())
