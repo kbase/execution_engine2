0fe40c512fb8f6f6fdede01a5ff14fc68720e146
Boris Sadkhin <boris@bortech.us>
2020-01-14 00:27:32 -0500

    Fixed client

--- lib/execution_engine2/utils/SlackUtils.py
+++ lib/execution_engine2/utils/SlackUtils.py
@@ -12,14 +12,14 @@ class SlackClient:
         message = (
             f"{username} has submitted job_id:{job_id} scheduler_id:{scheduler_id}"
         )
-        self.client.client.chat_postMessage(channel=self.channel, text=message)
+        self.client.chat_postMessage(channel=self.channel, text=message)
 
     def cancel_job_message(self, job_id, scheduler_id, termination_code):
         message = f"scheduler_id:{scheduler_id} job_id:{job_id} has been canceled due to {termination_code}"
-        self.client.client.chat_postMessage(channel=self.channel, text=message)
+        self.client.chat_postMessage(channel=self.channel, text=message)
 
     def finish_job_message(self, job_id, scheduler_id, finish_status, error_code=None):
         message = f"scheduler_id:{scheduler_id} job_id:{job_id} has ended with a status of {finish_status}"
         if error_code is not None:
             message += f" Error code is {error_code}"
-        self.client.client.chat_postMessage(channel=self.channel, text=message)
+        self.client.chat_postMessage(channel=self.channel, text=message)
