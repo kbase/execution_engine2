b0ffeefd6e3f7cb00504856777ca3ed99a95c43c
Tianhao-Gu <tgu@anl.gov>
2020-04-02 11:58:15 -0500

    make ts defaults to None

--- lib/execution_engine2/db/models/transfer/transfer_logs.py
+++ lib/execution_engine2/db/models/transfer/transfer_logs.py
@@ -95,7 +95,7 @@ class MigrateDatabases:
         convert input time into timestamp in epoch format
         """
         if not time_input:
-            return 0
+            return None
 
         try:
             if isinstance(time_input, str):  # input time_input as string
@@ -119,7 +119,7 @@ class MigrateDatabases:
             datetime.fromtimestamp(time_input)  # check current time_input is valid
         except Exception:
             print("Cannot convert time_input into timestamps: {}".format(time_input))
-            time_input = 0
+            time_input = None
 
         return time_input
 
