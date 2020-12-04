7276d9e1282203251e7f6da92f54d4983a5158cc
bio-boris <bioboris@github.com>
2020-12-04 16:18:38 -0600

    Add endpoint

--- lib/execution_engine2/sdk/SDKMethodRunner.py
+++ lib/execution_engine2/sdk/SDKMethodRunner.py
@@ -20,6 +20,7 @@ from installed_clients.WorkspaceClient import Workspace
 from installed_clients.authclient import KBaseAuth
 from lib.execution_engine2.authorization.workspaceauth import WorkspaceAuth
 from lib.execution_engine2.db.MongoUtil import MongoUtil
+from lib.execution_engine2.db.models.models import Job
 from lib.execution_engine2.exceptions import AuthError
 from lib.execution_engine2.sdk import (
     EE2Runjob,
@@ -34,7 +35,6 @@ from lib.execution_engine2.utils.Condor import Condor
 from lib.execution_engine2.utils.EE2Logger import get_logger
 from lib.execution_engine2.utils.KafkaUtils import KafkaClient
 from lib.execution_engine2.utils.SlackUtils import SlackClient
-from lib.execution_engine2.db.models.models import Job
 
 
 class JobPermissions(Enum):
@@ -316,7 +316,7 @@ class SDKMethodRunner:
         )
 
     def get_job_status_field(self, job_id, as_admin=False):
-        """ Authorization Required: Read """
+
         return self.get_jobs_status().get_job_status(job_id=job_id, as_admin=as_admin)
 
     def check_job_batch(
@@ -326,30 +326,33 @@ class SDKMethodRunner:
         exclude_fields=None,
         as_admin=False,
     ):
+        """ Authorization Required: Read """
+
         if as_admin is True:
             self.check_as_admin(requested_perm=JobPermissions.READ)
             check_permission = False
 
         if exclude_fields and "child_jobs" in exclude_fields:
-            raise ValueError(
-                "You silly goose. You can't exclude child jobs from this endpoint"
-            )
+            raise ValueError("You can't exclude child jobs from this endpoint")
 
         parent_job_status = self.get_jobs_status().check_job(
             job_id=parent_job_id,
             check_permission=check_permission,
             exclude_fields=exclude_fields,
         )
-        # child_job_ids = parent_job["child_job"]
-        # if children:
-        #     chil self.get_jobs_status().check_jobs(
-        #         job_ids=job_ids,
-        #         check_permission=check_permission,
-        #         exclude_fields=exclude_fields,
-        #         return_list=1,
-        #     )
-
-        return {"parent_job": parent_job_status, "child_jobs": []}
+        child_job_ids = parent_job_status.get("child_job_ids")
+        child_job_states = []
+        if child_job_ids:
+            child_job_states = self.get_jobs_status().check_jobs(
+                job_ids=child_job_ids,
+                check_permission=True,
+                exclude_fields=exclude_fields,
+                return_list=1,
+            )
+        return {
+            "parent_jobstate": parent_job_status,
+            "child_jobstates": child_job_states,
+        }
 
     def check_jobs(
         self,
