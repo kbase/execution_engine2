575069b9ad99fa41ed25c5b104f269fe625d4d0c
Gavin <gaprice@lbl.gov>
2021-01-20 16:24:53 -0800

    Correct spec, improve documentation
    Corrected / improved spec and spec documentation for date range methods

--- lib/execution_engine2/sdk/EE2Constants.py
+++ lib/execution_engine2/sdk/EE2Constants.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 from typing import Optional, NamedTuple
 
+# May want to make this configurable. Hardcoded for now as we want concierge data to be owned
+# by this user.
+# An alternative approach would be to configure a kbaseconcierge token in the config, and then
+# specify an auth2 role that allows users to replace their token with the kbaseconcierge token
+# when running jobs. Needs more thought.
 KBASE_CONCIERGE_USERNAME = "kbaseconcierge"
 CONCIERGE_CLIENTGROUP = "kbase_concierge"
 
