b074b38f6916cc67b5f356a7d38ed978440f40a9
Gavin <gaprice@lbl.gov>
2021-01-28 15:08:58 -0800

    I thought I ran pre-commmit on this but I guess not
    Also added pre-commit instructions

--- lib/execution_engine2/utils/clients.py
+++ lib/execution_engine2/utils/clients.py
@@ -1,6 +1,6 @@
-'''
+"""
 Contains the various clients EE2 needs to communicate with other services it depends on.
-'''
+"""
 
 # Note on testing - this class is not generally unit-testable, and is only tested fully in
 # integration tests.
@@ -20,12 +20,13 @@ from installed_clients.WorkspaceClient import Workspace
 
 
 class UserClientSet:
-    '''
+    """
     Clients required by EE2 for communicating with other services that need to be instantiated
     on a per user basis. Also contains the user credentials for ease of use.
-    '''
+    """
+
     def __init__(self, cfg: Dict[str, str], user_id: str, token: str):
-        '''
+        """
         Initialize the client set.
 
         cfg - the configuration dictionary
@@ -36,13 +37,13 @@ class UserClientSet:
 
         Expected keys in config:
         workspace-url - the URL of the kbase workspace service
-        '''
+        """
         if not user_id or not user_id.strip():
-            raise ValueError('user_id is required')
+            raise ValueError("user_id is required")
         if not token or not token.strip():
-            raise ValueError('token is required')
+            raise ValueError("token is required")
         if not cfg:
-            raise ValueError('cfg is required')
+            raise ValueError("cfg is required")
         self._user_id = user_id
         self._token = token
 
@@ -53,7 +54,7 @@ class UserClientSet:
         # if not ws_url or not ws_url.strip:
         #     raise ValueError('missing workspace-url key in configuration')
         # TODO Originally did the above check but caused 36 test failures so... meh for now.
-        self._workspace = Workspace(cfg['workspace-url'], token=token)
+        self._workspace = Workspace(cfg["workspace-url"], token=token)
         self._workspace_auth = WorkspaceAuth(user_id, self._workspace)
 
     # create_autospec can't mock instance variables, so we add some boilerplate
@@ -74,14 +75,14 @@ class UserClientSet:
 
 
 class ClientSet:
-    '''
+    """
     Clients required by EE2 for communicating with other services.
 
     These are not user-specific and can be reused throughout the application.
-    '''
+    """
 
     def __init__(self, cfg: Dict[str, str], cfg_path: str, debug: bool = False):
-        '''
+        """
         Initialize the client set from a configuration dictionary.
 
         cfg - the configuration dictionary
@@ -94,18 +95,19 @@ class ClientSet:
         catalog-token - a token to use with the catalog service. Ideally a service token
         kafka-host - the host string for a Kafka service
         slack-token - a token for contacting Slack
-        '''
+        """
         # TODO seems like it'd make sense to init Condor from a config dict like everything else
         self.condor = Condor(cfg_path)
-        self.catalog_utils = CatalogUtils(cfg['catalog-url'], cfg['catalog-token'])
-        auth_url = cfg['auth-url']
-        self.auth = KBaseAuth(auth_url=auth_url + '/api/legacy/KBase/Sessions/Login')
+        self.catalog_utils = CatalogUtils(cfg["catalog-url"], cfg["catalog-token"])
+        auth_url = cfg["auth-url"]
+        self.auth = KBaseAuth(auth_url=auth_url + "/api/legacy/KBase/Sessions/Login")
         # TODO using hardcoded roles for now to avoid possible bugs with mismatched cfg roles
         #       these should probably be configurable
         self.auth_admin = AdminAuthUtil(auth_url, [ADMIN_READ_ROLE, ADMIN_WRITE_ROLE])
 
         # KafkaClient has a nice error message when the arg is None
-        self.kafka_client = KafkaClient(cfg.get('kafka-host'))
+        self.kafka_client = KafkaClient(cfg.get("kafka-host"))
         # SlackClient handles None arguments
         self.slack_client = SlackClient(
-            cfg.get("slack-token"), debug=debug, endpoint=cfg.get("ee2-url"))
+            cfg.get("slack-token"), debug=debug, endpoint=cfg.get("ee2-url")
+        )
--- test/tests_for_utils/clients_test.py
+++ test/tests_for_utils/clients_test.py
@@ -8,12 +8,18 @@ from utils_shared.test_utils import assert_exception_correct
 
 
 def test_user_client_set_init_fail():
-    user_client_set_init_fail(None, 'foo', 'bar', ValueError('cfg is required'))
-    user_client_set_init_fail({}, 'foo', 'bar', ValueError('cfg is required'))
-    user_client_set_init_fail({'a': 'b'}, None, 'bar', ValueError('user_id is required'))
-    user_client_set_init_fail({'a': 'b'}, '    \t  ', 'bar', ValueError('user_id is required'))
-    user_client_set_init_fail({'a': 'b'}, 'foo', None, ValueError('token is required'))
-    user_client_set_init_fail({'a': 'b'}, 'foo', '    \t   ', ValueError('token is required'))
+    user_client_set_init_fail(None, "foo", "bar", ValueError("cfg is required"))
+    user_client_set_init_fail({}, "foo", "bar", ValueError("cfg is required"))
+    user_client_set_init_fail(
+        {"a": "b"}, None, "bar", ValueError("user_id is required")
+    )
+    user_client_set_init_fail(
+        {"a": "b"}, "    \t  ", "bar", ValueError("user_id is required")
+    )
+    user_client_set_init_fail({"a": "b"}, "foo", None, ValueError("token is required"))
+    user_client_set_init_fail(
+        {"a": "b"}, "foo", "    \t   ", ValueError("token is required")
+    )
 
 
 def user_client_set_init_fail(cfg, user, token, expected):
