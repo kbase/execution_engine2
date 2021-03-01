bc0693ecc80bf5e877c5cb3e8c0a586a72a06038
Gavin <gaprice@lbl.gov>
2021-03-01 12:27:11 -0800

    DRY up mock_utils.py

--- test/utils_shared/mock_utils.py
+++ test/utils_shared/mock_utils.py
@@ -12,15 +12,25 @@ from execution_engine2.utils.Condor import Condor
 from execution_engine2.sdk.EE2Constants import ADMIN_READ_ROLE, ADMIN_WRITE_ROLE
 from execution_engine2.utils.clients import ClientSet
 
-ALL_CLIENTS = [
-    KBaseAuth,
-    AdminAuthUtil,
-    Condor,
-    CatalogUtils,
-    KafkaClient,
-    MongoUtil,
-    SlackClient,
-]
+_CLASS_IMPLEMENTATION_BUILDERS = {
+    KBaseAuth: lambda config, cfgfile: KBaseAuth(
+        auth_url=config["auth-url"] + "/api/legacy/KBase/Sessions/Login"
+    ),
+    AdminAuthUtil: lambda config, cfgfile: AdminAuthUtil(
+        config["auth-url"], [ADMIN_READ_ROLE, ADMIN_WRITE_ROLE]
+    ),
+    Condor: lambda config, cfgfile: Condor(cfgfile),
+    CatalogUtils: lambda config, cfgfile: CatalogUtils(
+        config["catalog-url"], config["catalog-token"]
+    ),
+    KafkaClient: lambda config, cfgfile: KafkaClient(config["kafka-host"]),
+    MongoUtil: lambda config, cfgfile: MongoUtil(config),
+    SlackClient: lambda config, cfgfile: SlackClient(
+        config["slack-token"], debug=True, endpoint=config["ee2-url"]
+    ),
+}
+
+ALL_CLIENTS = _CLASS_IMPLEMENTATION_BUILDERS.keys()
 
 
 def get_client_mocks(config, config_path, *to_be_mocked):
@@ -35,58 +45,19 @@ def get_client_mocks(config, config_path, *to_be_mocked):
     Returns a dict of the class to the the class's mock or implementation as specified in
     the arguments.
     """
-
-    # could make a dict of methods to call on a non-mock build but that seems like overkill
-    # and almost as much code as this
-    if KBaseAuth in to_be_mocked:
-        kbase_auth = create_autospec(KBaseAuth, instance=True, spec_set=True)
-    else:
-        kbase_auth = KBaseAuth(
-            auth_url=config["auth-url"] + "/api/legacy/KBase/Sessions/Login"
-        )
-
-    if AdminAuthUtil in to_be_mocked:
-        authadmin = create_autospec(AdminAuthUtil, instance=True, spec_set=True)
-    else:
-        authadmin = AdminAuthUtil(
-            config["auth-url"], [ADMIN_READ_ROLE, ADMIN_WRITE_ROLE]
-        )
-
-    if Condor in to_be_mocked:
-        condor = create_autospec(Condor, instance=True, spec_set=True)
-    else:
-        condor = condor = Condor(config_path)
-
-    if CatalogUtils in to_be_mocked:
-        catutil = create_autospec(CatalogUtils, instance=True, spec_set=True)
-    else:
-        catutil = CatalogUtils(config["catalog-url"], config["catalog-token"])
-
-    if KafkaClient in to_be_mocked:
-        kafka = create_autospec(KafkaClient, instance=True, spec_set=True)
-    else:
-        kafka = KafkaClient(config["kafka-host"])
-
-    if MongoUtil in to_be_mocked:
-        mongo = create_autospec(MongoUtil, instance=True, spec_set=True)
-    else:
-        mongo = MongoUtil(config)
-
-    if SlackClient in to_be_mocked:
-        slack = create_autospec(SlackClient, instance=True, spec_set=True)
-    else:
-        slack = SlackClient(
-            config["slack-token"], debug=True, endpoint=config["ee2-url"]
-        )
-
-    cs = ClientSet(kbase_auth, authadmin, condor, catutil, kafka, mongo, slack)
-    return {
-        ClientSet: cs,
-        KBaseAuth: kbase_auth,
-        AdminAuthUtil: authadmin,
-        Condor: condor,
-        CatalogUtils: catutil,
-        KafkaClient: kafka,
-        MongoUtil: mongo,
-        SlackClient: slack,
-    }
+    ret = {}
+    for clazz in ALL_CLIENTS:
+        if clazz in to_be_mocked:
+            ret[clazz] = create_autospec(clazz, instance=True, spec_set=True)
+        else:
+            ret[clazz] = _CLASS_IMPLEMENTATION_BUILDERS[clazz](config, config_path)
+    ret[ClientSet] = ClientSet(
+        ret[KBaseAuth],
+        ret[AdminAuthUtil],
+        ret[Condor],
+        ret[CatalogUtils],
+        ret[KafkaClient],
+        ret[MongoUtil],
+        ret[SlackClient],
+    )
+    return ret
