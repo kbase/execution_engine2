9a9ae7f5a4dd4bd47eb9a73c1bccee2364a38703
Gavin <gaprice@lbl.gov>
2021-03-10 14:02:25 -0800

    Add job submission parameters class

--- lib/execution_engine2/sdk/job_submission_parameters.py
+++ lib/execution_engine2/sdk/job_submission_parameters.py
@@ -3,12 +3,20 @@ Parameters for submitting a job to a scheduler.
 """
 
 from maps import FrozenMap
-from typing import Dict
-from execution_engine2.utils.arg_processing import check_string as _check_string
+from typing import Dict, List
+from execution_engine2.utils.arg_processing import (
+    check_string as _check_string,
+    not_falsy as _not_falsy,
+    not_falsy_in_iterable as _not_falsy_in_iterable,
+)
+from execution_engine2.utils.user_info import UserCreds
+from execution_engine2.utils.application_info import AppInfo
 from execution_engine2.exceptions import IncorrectParamsException
 
 
-def _gt_zero(num: int, name: str) -> int:
+def _gt_zero(num: int, name: str, optional=False) -> int:
+    if num is None and optional:
+        return None
     if num is None or num < 1:
         raise IncorrectParamsException(f"{name} must be at least 1")
     return num
@@ -94,5 +102,106 @@ class JobRequirements:
                 self.as_user,
                 self.ignore_concurrency_limits,
                 self.scheduler_requirements,
+            )
+        )
+
+
+# move this function somewhere else?
+def _is_valid_UPA(upa: str) -> (str, bool):
+    # returns an empty string if not a valid upa
+    if upa is None or not upa.strip():
+        return '', False
+    parts = [p.strip() for p in upa.split('/')]
+    if not len(parts) == 3:
+        return '', False
+    for p in parts:
+        try:
+            int(p)
+        except ValueError:
+            return '', False
+    return '/'.join(parts), True
+
+
+class JobSubmissionParameters:
+    """
+    Parameters for submitting a job to a job scheduler.
+    """
+
+    def __init__(
+        self,
+        job_id: str,
+        app_info: AppInfo,
+        job_reqs: JobRequirements,
+        user_creds: UserCreds,
+        parent_job_id: str = None,
+        wsid: int = None,
+        source_ws_objects: List[str] = None,
+        debug_mode: bool = False,
+    ):
+        """
+        Create the parameters.
+
+        job_id - the ID of the job.
+        app_info - information about the application to be run.
+        job_reqs - requirements for the job.
+        user_creds - user credentials.
+        parent_job_id - the ID of the parent job to this job, if any.
+        wsid - the ID of the workspace with which the job is associated, if any.
+        source_ws_objects - workspace objects that are part of the job input.
+        debug_mode - whether to run the job in debug mode.
+        """
+        self.job_id = _check_string(job_id, "job_id")
+        self.app_info = _not_falsy(app_info, "app_info")
+        self.job_reqs = _not_falsy(job_reqs, "job_reqs")
+        self.user_creds = _not_falsy(user_creds, "user_creds")
+        self.parent_job_id = _check_string(
+            parent_job_id, "parent_job_id", optional=True
+        )
+        self.wsid = _gt_zero(wsid, "wsid", optional=True)
+        source_ws_objects = source_ws_objects if source_ws_objects else []
+        for i, ref in enumerate(source_ws_objects):
+            upa, is_valid = _is_valid_UPA(ref)
+            if not is_valid:
+                raise IncorrectParamsException(
+                    f"source_ws_objects index {i}, '{ref}', "
+                    + 'is not a valid Unique Permanent Address')
+            source_ws_objects[i] = upa
+        self.source_ws_objects = tuple(source_ws_objects)
+        self.debug_mode = debug_mode
+
+    def __eq__(self, other):
+        if type(self) == type(other):
+            return (
+                self.job_id,
+                self.app_info,
+                self.job_reqs,
+                self.user_creds,
+                self.parent_job_id,
+                self.wsid,
+                self.source_ws_objects,
+                self.debug_mode,
+            ) == (
+                other.job_id,
+                other.app_info,
+                other.job_reqs,
+                other.user_creds,
+                other.parent_job_id,
+                other.wsid,
+                other.source_ws_objects,
+                other.debug_mode,
+            )
+        return False
+
+    def __hash__(self):
+        return hash(
+            (
+                self.job_id,
+                self.app_info,
+                self.job_reqs,
+                self.user_creds,
+                self.parent_job_id,
+                self.wsid,
+                self.source_ws_objects,
+                self.debug_mode,
             )
         )
--- test/tests_for_sdkmr/job_submission_parameters_test.py
+++ test/tests_for_sdkmr/job_submission_parameters_test.py
@@ -1,5 +1,10 @@
 from pytest import raises
-from execution_engine2.sdk.job_submission_parameters import JobRequirements
+from execution_engine2.sdk.job_submission_parameters import (
+    JobRequirements,
+    JobSubmissionParameters,
+)
+from execution_engine2.utils.user_info import UserCreds
+from execution_engine2.utils.application_info import AppInfo
 from execution_engine2.exceptions import IncorrectParamsException
 from utils_shared.test_utils import assert_exception_correct
 
@@ -151,7 +156,7 @@ def _job_req_init_fail(cpus, mem, disk, cgroup, user, reqs, expected):
     assert_exception_correct(got.value, expected)
 
 
-def test_equals():
+def test_job_req_equals():
     c1 = "cligroupf"
     c1a = "cligroupf"
     c2 = "cligroupg"
@@ -190,7 +195,7 @@ def test_equals():
     assert JobRequirements(1, 1, 1, c1, t, u1, f, r1) != (1, 1, 1, c1a, t, u1a, f, r1a)
 
 
-def test_hash():
+def test_job_req_hash():
     # hashes will change from instance to instance of the python interpreter, and therefore
     # tests can't be written that directly test the hash value. See
     # https://docs.python.org/3/reference/datamodel.html#object.__hash__
@@ -228,3 +233,162 @@ def test_hash():
     assert hash(JobRequirements(1, 1, 1, c1, t, u1, f, r1)) != hash(
         JobRequirements(1, 1, 1, c1a, t, u1a, f, r2)
     )
+
+
+def test_job_sub_init_minimal():
+    jsp = JobSubmissionParameters(
+        "jobid",
+        AppInfo("a.b", "a/x"),
+        JobRequirements(6, 7, 4, "cligroup"),
+        UserCreds("user", "tokeytoken"),
+    )
+
+    assert jsp.job_id == "jobid"
+    assert jsp.app_info == AppInfo("a.b", "a/x")
+    assert jsp.job_reqs == JobRequirements(6, 7, 4, "cligroup")
+    assert jsp.user_creds == UserCreds("user", "tokeytoken")
+    assert jsp.parent_job_id is None
+    assert jsp.wsid is None
+    assert jsp.source_ws_objects == tuple()
+    assert jsp.debug_mode is False
+
+
+def test_job_sub_init_maximal():
+    jsp = JobSubmissionParameters(
+        "    jobid  \t  ",
+        AppInfo("a.b", "a/x"),
+        JobRequirements(6, 7, 4, "cligroup"),
+        UserCreds("user", "tokeytoken"),
+        "    parentid  \t   ",
+        1,
+        ['   1   /\t2   /   4', '6/7/8'],
+        True
+    )
+
+    assert jsp.job_id == "jobid"
+    assert jsp.app_info == AppInfo("a.b", "a/x")
+    assert jsp.job_reqs == JobRequirements(6, 7, 4, "cligroup")
+    assert jsp.user_creds == UserCreds("user", "tokeytoken")
+    assert jsp.parent_job_id == "parentid"
+    assert jsp.wsid == 1
+    assert jsp.source_ws_objects == ('1/2/4', '6/7/8')
+    assert jsp.debug_mode is True
+
+
+def test_job_sub_init_fail():
+    n = None
+    j = "jobby job job"
+    a = AppInfo("a.b", "a/x")
+    r = JobRequirements(6, 7, 4, "cligroup")
+    u = UserCreds("user", "tokeytoken")
+
+    _job_sub_init_fail(n, a, r, u, n, n, n, IncorrectParamsException(
+        "Missing input parameter: job_id"))
+    _job_sub_init_fail("  \t   ", a, r, u, n, n, n, IncorrectParamsException(
+        "Missing input parameter: job_id"))
+    _job_sub_init_fail(j, n, r, u, n, n, n, ValueError(
+        "app_info cannot be a value that evaluates to false"))
+    _job_sub_init_fail(j, a, n, u, n, n, n, ValueError(
+        "job_reqs cannot be a value that evaluates to false"))
+    _job_sub_init_fail(j, a, r, n, n, n, n, ValueError(
+        "user_creds cannot be a value that evaluates to false"))
+    # the only way to get parent id to to fail is with a control char
+    _job_sub_init_fail(j, a, r, u, "par\bent", n, n, IncorrectParamsException(
+        "parent_job_id contains control characters"))
+    _job_sub_init_fail(j, a, r, u, n, 0, n, IncorrectParamsException(
+        "wsid must be at least 1"))
+    _job_sub_init_fail(j, a, r, u, n, n, ['1/2/3', n], IncorrectParamsException(
+        "source_ws_objects index 1, 'None', is not a valid Unique Permanent Address"))
+    _job_sub_init_fail(j, a, r, u, n, n, ['1/2/3', "   \t  "], IncorrectParamsException(
+        "source_ws_objects index 1, '   \t  ', is not a valid Unique Permanent Address"))
+    for o in ['1/2', '1/2/', '/1/2', '1/2/3/4', 'x/2/3', '1/x/3', '1/2/x']:
+        _job_sub_init_fail(j, a, r, u, n, n, [o], IncorrectParamsException(
+            f"source_ws_objects index 0, '{o}', is not a valid Unique Permanent Address")
+        )
+
+
+def _job_sub_init_fail(jobid, appinfo, jobreq, usercred, parentid, wsid, wso, expected):
+    with raises(Exception) as got:
+        JobSubmissionParameters(jobid, appinfo, jobreq, usercred, parentid, wsid, wso)
+    assert_exception_correct(got.value, expected)
+
+
+def test_job_sub_equals():
+    j1 = "jobby job job"
+    j1a = "jobby job job"
+    j2 = "jobby job job JOB"
+    a1 = AppInfo("a.b", "a/x")
+    a1a = AppInfo("a.b", "a/x")
+    a2 = AppInfo("a.b", "a/y")
+    r1 = JobRequirements(6, 7, 4, "cligroup")
+    r1a = JobRequirements(6, 7, 4, "cligroup")
+    r2 = JobRequirements(6, 7, 4, "cligroup2")
+    u1 = UserCreds("user", "tokeytoken")
+    u1a = UserCreds("user", "tokeytoken")
+    u2 = UserCreds("user", "tokeytoken2")
+    p1 = "I'm so miserable and you just don't care"
+    p1a = "I'm so miserable and you just don't care"
+    p2 = "Oh do shut up Portia"
+    w1 = ['1/2/3']
+    w1a = ['1/2/3']
+    w2 = ['1/2/4']
+    t = True
+    f = False
+
+    JSP = JobSubmissionParameters
+
+    assert JSP(j1, a1, r1, u1) == JSP(j1a, a1a, r1a, u1a)
+    assert JSP(j1, a1, r1, u1, p1, 1, w1, t) == JSP(j1a, a1a, r1a, u1a, p1a, 1, w1a, t)
+
+    assert JSP(j1, a1, r1, u1) != JSP(j2, a1a, r1a, u1a)
+    assert JSP(j1, a1, r1, u1) != JSP(j1a, a2, r1a, u1a)
+    assert JSP(j1, a1, r1, u1) != JSP(j1a, a1a, r2, u1a)
+    assert JSP(j1, a1, r1, u1) != JSP(j1a, a1a, r1a, u2)
+    assert JSP(j1, a1, r1, u1) != (j1a, a1a, r1a, u1a)
+
+    assert JSP(j1, a1, r1, u1, p1, 1, w1, t) != JSP(j1a, a1a, r1a, u1a, p2, 1, w1a, t)
+    assert JSP(j1, a1, r1, u1, p1, 1, w1, t) != JSP(j1a, a1a, r1a, u1a, p1a, 2, w1a, t)
+    assert JSP(j1, a1, r1, u1, p1, 1, w1, t) != JSP(j1a, a1a, r1a, u1a, p1a, 1, w2, t)
+    assert JSP(j1, a1, r1, u1, p1, 1, w1, t) != JSP(j1a, a1a, r1a, u1a, p1a, 1, w1a, f)
+    assert JSP(j1, a1, r1, u1, p1, 1, w1, t) != (j1a, a1a, r1a, u1a, p1a, 1, w1a, t)
+
+
+def test_job_sub_hash():
+    # hashes will change from instance to instance of the python interpreter, and therefore
+    # tests can't be written that directly test the hash value. See
+    # https://docs.python.org/3/reference/datamodel.html#object.__hash__
+    j1 = "jobby job job"
+    j1a = "jobby job job"
+    j2 = "jobby job job JOB"
+    a1 = AppInfo("a.b", "a/x")
+    a1a = AppInfo("a.b", "a/x")
+    a2 = AppInfo("a.b", "a/y")
+    r1 = JobRequirements(6, 7, 4, "cligroup")
+    r1a = JobRequirements(6, 7, 4, "cligroup")
+    r2 = JobRequirements(6, 7, 4, "cligroup2")
+    u1 = UserCreds("user", "tokeytoken")
+    u1a = UserCreds("user", "tokeytoken")
+    u2 = UserCreds("user", "tokeytoken2")
+    p1 = "I'm so miserable and you just don't care"
+    p1a = "I'm so miserable and you just don't care"
+    p2 = "Oh do shut up Portia"
+    w1 = ['1/2/3']
+    w1a = ['1/2/3']
+    w2 = ['1/2/4']
+    t = True
+    f = False
+
+    JSP = JobSubmissionParameters
+
+    assert hash(JSP(j1, a1, r1, u1)) == hash(JSP(j1a, a1a, r1a, u1a ))
+    assert hash(JSP(j1, a1, r1, u1, p1, 1, w1, t)) == hash(JSP(j1a, a1a, r1a, u1a, p1a, 1, w1a, t))
+
+    assert hash(JSP(j1, a1, r1, u1)) != hash(JSP(j2, a1a, r1a, u1a))
+    assert hash(JSP(j1, a1, r1, u1)) != hash(JSP(j1a, a2, r1a, u1a))
+    assert hash(JSP(j1, a1, r1, u1)) != hash(JSP(j1a, a1a, r2, u1a))
+    assert hash(JSP(j1, a1, r1, u1)) != hash(JSP(j1a, a1a, r1a, u2))
+
+    assert hash(JSP(j1, a1, r1, u1, p1, 1, w1, t)) != hash(JSP(j1a, a1a, r1a, u1a, p2, 1, w1a, t))
+    assert hash(JSP(j1, a1, r1, u1, p1, 1, w1, t)) != hash(JSP(j1a, a1a, r1a, u1a, p1a, 2, w1a, t))
+    assert hash(JSP(j1, a1, r1, u1, p1, 1, w1, t)) != hash(JSP(j1a, a1a, r1a, u1a, p1a, 1, w2, t))
+    assert hash(JSP(j1, a1, r1, u1, p1, 1, w1, t)) != hash(JSP(j1a, a1a, r1a, u1a, p1a, 1, w1a, f))
