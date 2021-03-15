b95cb365f0fa45b54ed43f4deabe8cc069400c9e
Gavin <gaprice@lbl.gov>
2021-03-15 15:16:53 -0700

    Add job requirements class
    Hold information about resources and requirements for running a job.

---
+++ lib/execution_engine2/sdk/job_submission_parameters.py
@@ -0,0 +1,96 @@
+"""
+Parameters for submitting a job to a scheduler.
+"""
+
+from maps import FrozenMap
+from typing import Dict
+from execution_engine2.utils.arg_processing import check_string as _check_string
+from execution_engine2.exceptions import IncorrectParamsException
+
+
+def _gt_zero(num: int, name: str) -> int:
+    if num is None or num < 1:
+        raise IncorrectParamsException(f"{name} must be at least 1")
+    return num
+
+
+class JobRequirements:
+    """
+    Requirements for running a job on a scheduler.
+    """
+
+    def __init__(
+        self,
+        cpus: int,
+        memory_MB: int,
+        disk_GB: int,
+        client_group: str,
+        client_group_regex: bool = False,
+        as_user: str = None,
+        ignore_concurrency_limits: bool = False,
+        scheduler_requirements: Dict[str, str] = None,
+    ):
+        """
+        Create the job requirements.
+
+        cpus - the number of CPUs required for the job.
+        memory_MB - the amount of memory, in MB, required for the job.
+        disk_GB - the amount of disk space, in GB, required for the job.
+        client_group - the client group in which the job will run.
+        client_group_regex - whether to treat the client group string as a regular expression
+            that can match multiple client groups.
+        as_user - run the job as an alternate user; take the user's username.
+        ignore_concurrency_limits - allow the user to run this job even if the user's maximum
+            job count has already been reached.
+        scheduler_requirements - arbitrary requirements for the scheduler passed as key/value
+            pairs. Requires knowledge of the scheduler API.
+        """
+        self.cpus = _gt_zero(cpus, "CPU count")
+        self.memory_MB = _gt_zero(memory_MB, "memory in MB")
+        self.disk_GB = _gt_zero(disk_GB, "disk space in GB")
+        self.client_group = _check_string(client_group, "client_group")
+        self.client_group_regex = client_group_regex
+        self.as_user = _check_string(as_user, "as_user", optional=True)
+        self.ignore_concurrency_limits = ignore_concurrency_limits
+        sr = scheduler_requirements if scheduler_requirements else {}
+        for key, value in sr.items():
+            _check_string(key, "key in scheduler requirements structure")
+            _check_string(value, f"value for key '{key}' in scheduler requirements structure")
+        self.scheduler_requirements = FrozenMap(sr)
+
+    def __eq__(self, other):
+        if type(self) == type(other):
+            return (
+                self.cpus,
+                self.memory_MB,
+                self.disk_GB,
+                self.client_group,
+                self.client_group_regex,
+                self.as_user,
+                self.ignore_concurrency_limits,
+                self.scheduler_requirements,
+            ) == (
+                other.cpus,
+                other.memory_MB,
+                other.disk_GB,
+                other.client_group,
+                other.client_group_regex,
+                other.as_user,
+                other.ignore_concurrency_limits,
+                other.scheduler_requirements,
+            )
+        return False
+
+    def __hash__(self):
+        return hash(
+            (
+                self.cpus,
+                self.memory_MB,
+                self.disk_GB,
+                self.client_group,
+                self.client_group_regex,
+                self.as_user,
+                self.ignore_concurrency_limits,
+                self.scheduler_requirements,
+            )
+        )
---
+++ test/tests_for_sdkmr/job_submission_parameters_test.py
@@ -0,0 +1,139 @@
+from pytest import raises
+from execution_engine2.sdk.job_submission_parameters import JobRequirements
+from execution_engine2.exceptions import IncorrectParamsException
+from utils_shared.test_utils import assert_exception_correct
+
+
+def test_job_req_init_minimal():
+    jr = JobRequirements(1, 1, 1, "njs")
+
+    assert jr.cpus == 1
+    assert jr.memory_MB == 1
+    assert jr.disk_GB == 1
+    assert jr.client_group == "njs"
+    assert jr.client_group_regex is False
+    assert jr.as_user is None
+    assert jr.ignore_concurrency_limits is False
+    assert jr.scheduler_requirements == {}
+
+
+def test_job_req_init_maximal():
+    jr = JobRequirements(
+        6, 7, 8, "bigmemlong", True, 'someuser', True, {'proc': 'x286', 'maxmem': '640k'}
+    )
+
+    assert jr.cpus == 6
+    assert jr.memory_MB == 7
+    assert jr.disk_GB == 8
+    assert jr.client_group == "bigmemlong"
+    assert jr.client_group_regex is True
+    assert jr.as_user == 'someuser'
+    assert jr.ignore_concurrency_limits is True
+    assert jr.scheduler_requirements == {'proc': 'x286', 'maxmem': '640k'}
+
+
+def test_job_req_init_fail():
+    n = None
+    _job_req_init_fail(n, 1, 1, "f", n, n, IncorrectParamsException(
+        'CPU count must be at least 1'))
+    _job_req_init_fail(0, 1, 1, "f", n, n, IncorrectParamsException(
+        'CPU count must be at least 1'))
+    _job_req_init_fail(1, n, 1, "f", n, n, IncorrectParamsException(
+        'memory in MB must be at least 1'))
+    _job_req_init_fail(1, 0, 1, "f", n, n, IncorrectParamsException(
+        'memory in MB must be at least 1'))
+    _job_req_init_fail(1, 1, n, "f", n, n, IncorrectParamsException(
+        'disk space in GB must be at least 1'))
+    _job_req_init_fail(1, 1, 0, "f", n, n, IncorrectParamsException(
+        'disk space in GB must be at least 1'))
+    _job_req_init_fail(1, 1, 1, n, n, n, IncorrectParamsException(
+        'Missing input parameter: client_group'))
+    _job_req_init_fail(1, 1, 1, "  \t    ", n, n, IncorrectParamsException(
+        'Missing input parameter: client_group'))
+    # as_user is optional, so this is the only possible failure mode
+    _job_req_init_fail(1, 1, 1, "f", "user\tname", n, IncorrectParamsException(
+        'as_user contains control characters'))
+    _job_req_init_fail(1, 1, 1, "f", n, {n: "a"}, IncorrectParamsException(
+        'Missing input parameter: key in scheduler requirements structure'))
+    _job_req_init_fail(1, 1, 1, "f", n, {"    \t   ": "a"}, IncorrectParamsException(
+        'Missing input parameter: key in scheduler requirements structure'))
+    _job_req_init_fail(1, 1, 1, "f", n, {"a": n}, IncorrectParamsException(
+        "Missing input parameter: value for key 'a' in scheduler requirements structure"))
+    _job_req_init_fail(1, 1, 1, "f", n, {"a": "    \t   "}, IncorrectParamsException(
+        "Missing input parameter: value for key 'a' in scheduler requirements structure"))
+
+
+def _job_req_init_fail(cpus, mem, disk, cgroup, user, reqs, expected):
+    with raises(Exception) as got:
+        JobRequirements(cpus, mem, disk, cgroup, False, user, False, reqs)
+    assert_exception_correct(got.value, expected)
+
+
+def test_equals():
+    c1 = "cligroupf"
+    c1a = "cligroupf"
+    c2 = "cligroupg"
+    t = True
+    f = False
+    u1 = "user1"
+    u1a = "user1"
+    u2 = "user2"
+    r1 = {"a": "b"}
+    r1a = {"a": "b"}
+    r2 = {"a": "c"}
+
+    assert JobRequirements(1, 1, 1, c1) == JobRequirements(1, 1, 1, c1a)
+    assert JobRequirements(1, 1, 1, c1, t, u1, f, r1) == JobRequirements(
+        1, 1, 1, c1a, t, u1a, f, r1a)
+
+    assert JobRequirements(1, 1, 1, c1) != JobRequirements(2, 1, 1, c1a)
+    assert JobRequirements(1, 1, 1, c1) != JobRequirements(1, 2, 1, c1a)
+    assert JobRequirements(1, 1, 1, c1) != JobRequirements(1, 1, 2, c1a)
+    assert JobRequirements(1, 1, 1, c1) != JobRequirements(1, 1, 1, c2)
+    assert JobRequirements(1, 1, 1, c1) != (1, 1, 1, c1)
+
+    assert JobRequirements(1, 1, 1, c1, t, u1, f, r1) != JobRequirements(
+        1, 1, 1, c1a, f, u1a, f, r1a)
+    assert JobRequirements(1, 1, 1, c1, t, u1, f, r1) != JobRequirements(
+        1, 1, 1, c1a, t, u2, f, r1a)
+    assert JobRequirements(1, 1, 1, c1, t, u1, f, r1) != JobRequirements(
+        1, 1, 1, c1a, t, u1a, t, r1a)
+    assert JobRequirements(1, 1, 1, c1, t, u1, f, r1) != JobRequirements(
+        1, 1, 1, c1a, t, u1a, f, r2)
+    assert JobRequirements(1, 1, 1, c1, t, u1, f, r1) != (
+        1, 1, 1, c1a, t, u1a, f, r1a)
+
+
+def test_hash():
+    # hashes will change from instance to instance of the python interpreter, and therefore
+    # tests can't be written that directly test the hash value. See
+    # https://docs.python.org/3/reference/datamodel.html#object.__hash__
+    c1 = "cligroupf"
+    c1a = "cligroupf"
+    c2 = "cligroupg"
+    t = True
+    f = False
+    u1 = "user1"
+    u1a = "user1"
+    u2 = "user2"
+    r1 = {"a": "b"}
+    r1a = {"a": "b"}
+    r2 = {"a": "c"}
+
+    assert hash(JobRequirements(1, 1, 1, c1)) == hash(JobRequirements(1, 1, 1, c1a))
+    assert hash(JobRequirements(1, 1, 1, c1, t, u1, f, r1)) == hash(JobRequirements(
+        1, 1, 1, c1a, t, u1a, f, r1a))
+
+    assert hash(JobRequirements(1, 1, 1, c1)) != hash(JobRequirements(2, 1, 1, c1a))
+    assert hash(JobRequirements(1, 1, 1, c1)) != hash(JobRequirements(1, 2, 1, c1a))
+    assert hash(JobRequirements(1, 1, 1, c1)) != hash(JobRequirements(1, 1, 2, c1a))
+    assert hash(JobRequirements(1, 1, 1, c1)) != hash(JobRequirements(1, 1, 1, c2))
+
+    assert hash(JobRequirements(1, 1, 1, c1, t, u1, f, r1)) != hash(JobRequirements(
+        1, 1, 1, c1a, f, u1a, f, r1a))
+    assert hash(JobRequirements(1, 1, 1, c1, t, u1, f, r1)) != hash(JobRequirements(
+        1, 1, 1, c1a, t, u2, f, r1a))
+    assert hash(JobRequirements(1, 1, 1, c1, t, u1, f, r1)) != hash(JobRequirements(
+        1, 1, 1, c1a, t, u1a, t, r1a))
+    assert hash(JobRequirements(1, 1, 1, c1, t, u1, f, r1)) != hash(JobRequirements(
+        1, 1, 1, c1a, t, u1a, f, r2))
