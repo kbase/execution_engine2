1c500af066539bbc608b794ca5f72003f7ff59d3
Gavin <gaprice@lbl.gov>
2021-03-21 14:48:36 -0700

    run black

--- test/tests_for_utils/job_requirements_resolver_test.py
+++ test/tests_for_utils/job_requirements_resolver_test.py
@@ -310,8 +310,14 @@ def _normalize_job_reqs_fail(reqs, source, req_all_res, expected):
 def test_get_requirements_type_standard():
     grt = JobRequirementsResolver.get_requirements_type
     assert grt() == RequirementsType.STANDARD
-    assert grt(None, None, None, None, None, None, None, None, None) == RequirementsType.STANDARD
-    assert grt(None, None, None, None, None, None, False, {}, False) == RequirementsType.STANDARD
+    assert (
+        grt(None, None, None, None, None, None, None, None, None)
+        == RequirementsType.STANDARD
+    )
+    assert (
+        grt(None, None, None, None, None, None, False, {}, False)
+        == RequirementsType.STANDARD
+    )
 
 
 def test_get_requirements_type_processing():
@@ -326,31 +332,37 @@ def test_get_requirements_type_processing():
     assert grt(scheduler_requirements={"a": "b"}) == RequirementsType.PROCESSING
     assert grt(debug_mode=True) == RequirementsType.PROCESSING
 
-    assert grt(
-        cpus=4,
-        memory_MB=2,
-        disk_GB=8,
-        client_group="yay",
-        client_group_regex=True,
-        ignore_concurrency_limits=True,
-        debug_mode=True,
-    ) == RequirementsType.PROCESSING
+    assert (
+        grt(
+            cpus=4,
+            memory_MB=2,
+            disk_GB=8,
+            client_group="yay",
+            client_group_regex=True,
+            ignore_concurrency_limits=True,
+            debug_mode=True,
+        )
+        == RequirementsType.PROCESSING
+    )
 
 
 def test_get_requirements_type_billing():
     grt = JobRequirementsResolver.get_requirements_type
     assert grt(bill_to_user="foo") == RequirementsType.BILLING
 
-    assert grt(
-        cpus=4,
-        memory_MB=2,
-        disk_GB=8,
-        client_group="yay",
-        client_group_regex=True,
-        bill_to_user="can I buy you a drink?",
-        ignore_concurrency_limits=True,
-        debug_mode=True,
-    ) == RequirementsType.BILLING
+    assert (
+        grt(
+            cpus=4,
+            memory_MB=2,
+            disk_GB=8,
+            client_group="yay",
+            client_group_regex=True,
+            bill_to_user="can I buy you a drink?",
+            ignore_concurrency_limits=True,
+            debug_mode=True,
+        )
+        == RequirementsType.BILLING
+    )
 
 
 def test_get_requirements_type_fail():
@@ -361,11 +373,25 @@ def test_get_requirements_type_fail():
     _grtf = _get_requirements_type_fail
     _grtf(0, n, n, n, n, IncorrectParamsException("CPU count must be at least 1"))
     _grtf(n, 0, n, n, n, IncorrectParamsException("memory in MB must be at least 1"))
-    _grtf(n, n, 0, n, n, IncorrectParamsException("disk space in GB must be at least 1"))
-    _grtf(n, n, n, "    \t   ", n, IncorrectParamsException(
-        "Missing input parameter: client_group"))
-    _grtf(n, n, n, n, "   \bfoo   ", IncorrectParamsException(
-        "bill_to_user contains control characters"))
+    _grtf(
+        n, n, 0, n, n, IncorrectParamsException("disk space in GB must be at least 1")
+    )
+    _grtf(
+        n,
+        n,
+        n,
+        "    \t   ",
+        n,
+        IncorrectParamsException("Missing input parameter: client_group"),
+    )
+    _grtf(
+        n,
+        n,
+        n,
+        n,
+        "   \bfoo   ",
+        IncorrectParamsException("bill_to_user contains control characters"),
+    )
     # note there are no invalid values for client_group_regex, ignore_concurrentcy_limits,
     # and debug_mode
 
@@ -373,5 +399,6 @@ def test_get_requirements_type_fail():
 def _get_requirements_type_fail(cpus, mem, disk, cg, btu, expected):
     with raises(Exception) as got:
         JobRequirementsResolver.get_requirements_type(
-            cpus, mem, disk, cg, False, btu, False, False)
+            cpus, mem, disk, cg, False, btu, False, False
+        )
     assert_exception_correct(got.value, expected)
