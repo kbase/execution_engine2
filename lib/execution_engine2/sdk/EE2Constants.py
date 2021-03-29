4984ef0c8c60940663b0332508d2c834b4a3e8a2
MrCreosote <MrCreosote@users.noreply.github.com>
2021-03-29 14:34:23 -0700

    DATAUP-389 - add a job requirements resolver, pt 3 of 4 (#340)
    * Job resolver: add initialization & spec parsing
    
    * run black
    
    * typo

--- lib/execution_engine2/sdk/EE2Constants.py
+++ lib/execution_engine2/sdk/EE2Constants.py
@@ -9,6 +9,10 @@ from typing import Optional, NamedTuple
 KBASE_CONCIERGE_USERNAME = "kbaseconcierge"
 CONCIERGE_CLIENTGROUP = "kbase_concierge"
 
+EE2_CONFIG_SECTION = "execution_engine2"
+EE2_DEFAULT_SECTION = "DEFAULT"
+EE2_DEFAULT_CLIENT_GROUP = "default_client_group"
+
 # these also probably should be configurable.
 ADMIN_READ_ROLE = "EE2_ADMIN_RO"
 ADMIN_WRITE_ROLE = "EE2_ADMIN"
