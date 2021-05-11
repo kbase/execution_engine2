f921c519995de82e4868489b4cc26637c2fb5eba
bio-boris <bio-boris@github.com>
2021-05-11 12:45:40 -0500

    Revert ee2server

--- lib/execution_engine2/execution_engine2Server.py
+++ lib/execution_engine2/execution_engine2Server.py
@@ -1,4 +1,4 @@
-lib / execution_engine2 / execution_engine2Server.py  #!/usr/bin/env python
+#!/usr/bin/env pythonlib/execution_engine2/execution_engine2Server.py
 # -*- coding: utf-8 -*-
 import datetime
 import json
@@ -122,12 +122,10 @@ class JSONRPCServiceCustom(JSONRPCService):
             newerr = JSONServerError()
             newerr.trace = traceback.format_exc()
             if len(e.args) == 1:
-                # THIS WAS CHANGED INTENTIONALLY - if you recompile please restore.
-                # repr adds single quotes around string arguments which is not what we want.
-                if type(e.args[0]) == str:
-                    newerr.data = e.args[0]
-                else:
-                    newerr.data = repr(e.args[0])
+                newerr.data = repr(e.args[0])
+            else:
+                newerr.data = repr(e.args)
+            raise newerr
         return result
 
     def call_py(self, ctx, jsondata):
