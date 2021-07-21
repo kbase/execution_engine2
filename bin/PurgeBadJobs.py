724e8cc076c4f4669304fd5bf0e3853daeff2bfa
bio-boris <bio-boris@github.com>
2021-07-20 22:59:19 -0500

    skeleton

--- bin/PurgeBadJobs.py
+++ bin/PurgeBadJobs.py
@@ -32,6 +32,7 @@ def cancel_jobs_stuck_in_queue():
     queue_threshold_days = 14
     d = datetime.today() - timedelta(days=queue_threshold_days + 1)
     stuck_jobs = db.ee2.find({"status": "queued", "queued": {"$gt": d.timestamp()})
+    logger.info(f"Found {len(stuck_jobs)} jobs that were stuck in the queue state over {queue_threshold_days} days")
     for record in stuck_jobs:
         cjp = {'as_admin': True, 'job_id': str(record.id),
                'terminated_code': TerminatedCode.terminated_by_automation.value}
@@ -42,13 +43,18 @@ def cancel_created():
     """
     For jobs that are not batch jobs, and have been in the created state for more than 5 minutes, uh oh, spagettio, time to go
     """
+    created_threshold_minutes = 5
+    d = datetime.today().timestamp() - 5
+    stuck_jobs = db.ee2.find({"status": "created", "_id": {"$gt": d)
+    logger.info(f"Found {len(stuck_jobs)} jobs that were stuck in the created state for over {created_threshold_minutes} mins")
+    for record in stuck_jobs:
+        cjp = {'as_admin': True, 'job_id': str(record.id),
+               'terminated_code': TerminatedCode.terminated_by_automation.value}
+        ee2.cancel_job(params=cjp)
+    slack_client.cancel_job_message(job_id=str(record.id),scheduler_id=record.scheduler_id)
 
 def clean_retried_jobs():
     """Clean up jobs that couldn't finish the retry lifecycle """
-
-
-def clean
-
 
 def purge():
     cancel_jobs_stuck_in_queue()
