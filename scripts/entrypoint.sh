19ec87b41617bf7cbfb0d0e6c8c9079f9a3f202d
bio-boris <bio-boris@github.com>
2021-07-26 16:37:43 -0500

    Fix env

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -19,7 +19,7 @@ if [ $# -eq 0 ] ; then
   mkdir /condor_shared/cluster_logs && chown kbase /condor_shared/cluster_logs
 
   # Save ENV Variables to file for cron
-  printenv > /etc/environment && chmod a+rw /etc/environment
+  env | sed 's/=\(.*\)/="\1"/' > /etc/environment && chmod a+rw /etc/environment
   service cron start
 
   sh ./scripts/start_server.sh
