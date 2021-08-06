04a25e24c8a4599975da8bcc55d6491d0a2637b8
bio-boris <bio-boris@github.com>
2021-08-06 13:22:38 -0500

    Dedupe and fail

--- lib/execution_engine2/sdk/EE2Runjob.py
+++ lib/execution_engine2/sdk/EE2Runjob.py
@@ -647,45 +647,35 @@ class EE2RunJob:
         if not job_ids:
             raise ValueError("No job_ids provided to retry")
 
-        offending_ids = defaultdict(int)
-        for job_id in job_ids:
-            if job_ids.count(job_id) > 1:
-                offending_ids[job_id] += 1
-
-        if offending_ids.keys():
-            raise ValueError(
-                f"Retry of the same id in the same request is not supported."
-                f" Offending ids:{list(offending_ids.keys())} "
-            )
+        # dedupe job ids
+        job_ids = list(set(job_ids))
 
         # Check all inputs before attempting to start submitting jobs
         retried_jobs = []
-        jobs = []
-        batch_jobs = []
         for job_id in job_ids:
+            # Check for presubmission failures
             try:
                 job, batch_job = self._validate_retry_presubmit(
                     job_id=job_id, as_admin=as_admin
                 )
-                jobs.append(job)
-                batch_jobs.append(batch_job)
             except Exception as e:
-                raise RetryFailureException(e)
-
-        # Submit all of the collected jobs
-        for i, job_id in enumerate(job_ids):
+                # Collect the presubmit error and don't submit the job
+                retried_jobs.append({"job_id": job_id, "error": f"{e}"})
+                continue
+            # Presubmit worked, write to the db and submit
             try:
                 retried_jobs.append(
                     self._retry(
                         job_id=job_id,
-                        job=jobs[i],
-                        batch_job=batch_jobs[i],
+                        job=job,
+                        batch_job=batch_job,
                         as_admin=as_admin,
                     )
                 )
             except Exception as e:
                 retried_jobs.append({"job_id": job_id, "error": f"{e}"})
-        return retried_jobs
+
+            return retried_jobs
 
     @staticmethod
     def _get_run_job_params_from_existing_job(job: Job, user_id: str) -> Dict:
