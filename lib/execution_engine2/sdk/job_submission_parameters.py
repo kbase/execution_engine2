114b5ae5c01361767a1c34b89d7956649d1ad389
Boris <bio-boris@github.com>
2024-07-26 15:13:19 -0500

    Fix E721

--- lib/execution_engine2/sdk/job_submission_parameters.py
+++ lib/execution_engine2/sdk/job_submission_parameters.py
@@ -124,9 +124,11 @@ class JobRequirements:
             client_group,
             None if client_group_regex is None else bool(client_group_regex),
             _check_string(bill_to_user, "bill_to_user", optional=True),
-            None
-            if ignore_concurrency_limits is None
-            else bool(ignore_concurrency_limits),
+            (
+                None
+                if ignore_concurrency_limits is None
+                else bool(ignore_concurrency_limits)
+            ),
             cls._check_scheduler_requirements(scheduler_requirements),
             None if debug_mode is None else bool(debug_mode),
         )
@@ -145,7 +147,7 @@ class JobRequirements:
         )
 
     def __eq__(self, other):
-        if type(self) == type(other): # noqa E721
+        if type(self) == type(other):  # noqa E721
             return self._params() == (
                 other.cpus,
                 other.memory_MB,
@@ -238,7 +240,7 @@ class JobSubmissionParameters:
         )
 
     def __eq__(self, other):
-        if type(self) == type(other): # noqa E721
+        if type(self) == type(other):  # noqa E721
             return self._params() == (
                 other.job_id,
                 other.app_info,
